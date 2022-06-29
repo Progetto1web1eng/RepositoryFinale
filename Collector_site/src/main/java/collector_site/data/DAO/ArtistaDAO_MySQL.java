7b1e4cecd34a0985c202fff59b479bfd7aa9acb5
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-06-29 12:41:41 +0200

    aggiunti metodi in ArtistaDAO_MySQL

--- Collector_site/src/main/java/collector_site/data/DAO/ArtistaDAO_MySQL.java
+++ Collector_site/src/main/java/collector_site/data/DAO/ArtistaDAO_MySQL.java
@@ -44,6 +44,8 @@ public class ArtistaDAO_MySQL extends DAO implements ArtistaDao {
     private PreparedStatement deleteArtista;
     private PreparedStatement getArtistaById;
     private PreparedStatement getArtisti;
+    private PreparedStatement getArtistaByDisco;
+    private PreparedStatement getArtistiByGruppoMusicale;
 
     public ArtistaDAO_MySQL(DataLayer d) {
         super(d);
@@ -58,6 +60,8 @@ public class ArtistaDAO_MySQL extends DAO implements ArtistaDao {
             deleteArtista = connection.prepareStatement("DELETE FROM artista WHERE ID=?"); 
             getArtistaById = connection.prepareStatement("SELECT * FROM artista WHERE ID=?");
             getArtisti = connection.prepareStatement("SELECT ID AS IDartista FROM artista");
+            getArtistaByDisco = connection.prepareStatement("SELECT IDartista FROM incide WHERE IDdisco=?");
+            getArtistiByGruppoMusicale = connection.prepareStatement("SELECT ID, IDruolo FROM artista WHERE IDgruppoMusicale=?");
         } catch (SQLException ex) {
             throw new DataException("Error initializing Artista data layer", ex);
         }
@@ -70,6 +74,8 @@ public class ArtistaDAO_MySQL extends DAO implements ArtistaDao {
             deleteArtista.close();
             getArtistaById.close();
             getArtisti.close();
+            getArtistaByDisco.close();
+            getArtistiByGruppoMusicale.close();
         } catch (SQLException ex) {
         }
         super.destroy();
@@ -89,8 +95,8 @@ public class ArtistaDAO_MySQL extends DAO implements ArtistaDao {
             artista.setKey(rs.getInt("ID"));
             artista.setNomeDarte(rs.getString("nomeDarte"));
             // Ruolo è un enumerazione
-            artista.setRuolo(Ruolo.values()[rs.getInt("IDruolo")]);
-            
+            // CHECK
+            //artista.setRuolo(Ruolo.values()[rs.getInt("IDruolo")]);
         } catch (SQLException ex) {
             throw new DataException("Unable to create Artista object form ResultSet", ex);
         }
@@ -104,7 +110,30 @@ public class ArtistaDAO_MySQL extends DAO implements ArtistaDao {
 
     @Override
     public Artista getArtistaById(int id) throws DataException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Artista artista = null;
+        
+        //prima vediamo se l'oggetto è già stato caricato
+        if (dataLayer.getCache().has(Artista.class, id)) {
+            artista = dataLayer.getCache().get(Artista.class, id);
+        } else {
+            //altrimenti lo carichiamo dal database
+            try {
+                getArtistaById.setInt(1, id);
+                try (ResultSet rs = getArtistaById.executeQuery()) {
+                    if (rs.next()) {
+                        artista = createArtista(rs);
+                        // si controlla se l'artista in questione è un gruppo musicale e se sì, si aggiungono
+                        // i suoi componenti
+                        artista = getArtistiByGruppoMusicale(artista);
+                        //e lo mettiamo anche nella cache
+                        dataLayer.getCache().add(Artista.class, artista);
+                    }
+                }
+            } catch (SQLException ex) {
+                throw new DataException("Unable to load Artista by ID", ex);
+            }
+        }
+        return artista;
     }
 
     @Override
@@ -118,36 +147,45 @@ public class ArtistaDAO_MySQL extends DAO implements ArtistaDao {
     }
 
     @Override
-    public void storeArtista(Artista artista) throws DataException {
-        /*
+    public void storeArtista(Artista artista, Integer idGruppoMusicale) throws DataException {
+        // si assume che il sistema permetta soltanto lo store e non l'update dell'Artista
+        
         try {
-            
-            // CREAZIONE DISCO
             if ("".equals(artista.getNomeDarte())) {
                 return;
             }
             
-            storeArtista.setString(2, artista.getNomeDarte());
+            storeArtista.setString(1, artista.getNomeDarte());
             
-            // per inserire i valori degli attributi IDruolo e ruolo nella tabella Artista
-            if (artista.getRuolo() == null) {
-                // IDruolo
-                storeArtista.setNull(3, java.sql.Types.SMALLINT);
-                // ruolo
-                storeArtista.setNull(4, java.sql.Types.VARCHAR);
+            if (artista.getComponenti().size() > 1 || artista.getComponenti() == null) {
+                // caso in cui si crea un gruppo musicale oppure un singolo Artista
+
+                // inserimento valore nullo nell'attributo IDRuolo in tabella Artista
+                storeArtista.setNull(2, java.sql.Types.SMALLINT);
+                // inserimento valore nullo nell'attributo ruolo in tabella Artista
+                storeArtista.setNull(3, java.sql.Types.VARCHAR);
+                // inserimento valore nullo nell'attributo IDgruppoMusicale in tabella Artista
+                storeArtista.setNull(4, java.sql.Types.SMALLINT);
+               
+                if (artista.getComponenti().size() > 1) {
+                    // caso in cui si crea un gruppo musicale
+                    for (Artista a : artista.getComponenti()) {
+                        Integer idGruppo = artista.getKey();
+                        // ricorsione che permette di aggiungere uno ad uno ciascun componente del gruppo 
+                        // musicale
+                        storeArtista(a,idGruppo);
+                    } 
+                }
+                
             } else {
+                // caso in cui si inserisce un componente di un gruppo musicale
+                
                 String ruolo = artista.getRuolo().toString();
-                // IDruolo
-                storeArtista.setInt(3, Ruolo.valueOf(ruolo).ordinal());
-                // ruolo
-                storeArtista.setString(4, ruolo);
-            }
-            
-            // per inserire il valore dell'attributo IDgruppoMusicale nella tabella Artista
-            if (artista.getGruppoMusicale() == null) {
-                storeArtista.setNull(5, java.sql.Types.SMALLINT);
-            } else {
-                storeArtista.setInt(5, artista.getGruppoMusicale().getKey());
+                // per inserire i valori degli attributi IDruolo e ruolo nella tabella Artista
+                storeArtista.setInt(2, Ruolo.valueOf(ruolo).ordinal());
+                storeArtista.setString(3, ruolo);
+                
+                storeArtista.setInt(4, idGruppoMusicale);
             }
             
             if (storeArtista.executeUpdate() == 1) {
@@ -171,8 +209,55 @@ public class ArtistaDAO_MySQL extends DAO implements ArtistaDao {
            
         } catch (SQLException ex) {
             throw new DataException("Unable to store Artista", ex);
-        }*/
+        }
     } 
+
+    @Override
+    public Artista getArtistaByDisco(Disco disco) throws DataException {
+        Artista artista = null;
+        
+        try {
+            getArtistaByDisco.setInt(1, disco.getKey());
+            try (ResultSet rs = getArtistaByDisco.executeQuery()) {
+                if (rs.next()) {
+                    artista = getArtistaById(rs.getInt("IDartista"));
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataException("Unable to load Artista by Disco", ex);
+        }
+        return artista;
+    }
+
+    @Override
+    public Artista getArtistiByGruppoMusicale(Artista artista) throws DataException {
+        List<Artista> componenti = new ArrayList<Artista>();
+        
+        try{
+            getArtistiByGruppoMusicale.setInt(1, artista.getKey());
+            try(ResultSet rs = getArtistiByGruppoMusicale.executeQuery()){
+                while (rs.next()){
+                    Artista componente = getArtistaById(rs.getInt("ID"));
+
+                    componente.setRuolo(Ruolo.values()[rs.getInt("IDruolo")]);
+                    componenti.add(componente);
+                }
+            }
+        }catch(SQLException ex){
+            throw new DataException("Unable to load Artista by gruppo musicale");
+        }
+        
+        if (componenti.size() > 0) {
+            // caso in cui l'oggetto {O} passato come parametro è un gruppo musicale ==> è necessario 
+            // aggiungere ad {0} i componenti del gruppo musicale
+            artista.setComponenti(componenti);
+            return artista;
+        }
+        
+        return artista; // caso in cui l'oggetto passato come parametro non è un gruppo musicale ==> non è 
+                        // necessario aggiungere ad {0} i componenti del gruppo musicale in quanto {o} 
+                        // rappresenta un singolo artista e non un gruppo musicale
+    }
 }
 
 

b5004fecc893fb8326d90baadf262aac59196ac6
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-07-03 16:15:12 +0200

    modificato getDischiByCollezionista

--- Collector_site/src/main/java/collector_site/data/DAO/DiscoDAO_MySQL.java
+++ Collector_site/src/main/java/collector_site/data/DAO/DiscoDAO_MySQL.java
@@ -538,7 +538,9 @@ public class DiscoDAO_MySQL extends DAO implements DiscoDao {
             getDischiByCollezionista.setInt(1, collezionista.getKey());
             
             try(ResultSet rs = getDischiByCollezionista.executeQuery()){
-                result.add(getDisco(rs.getInt("c.IDdisco")));
+                while(rs.next()) {
+                    result.add(getDisco(rs.getInt("c.IDdisco")));
+                }
             }
             
         }catch(SQLException ex){

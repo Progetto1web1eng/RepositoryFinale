d2ae82605ded08ea1ca323b59b4e77b9213d3fcd
Stefano Pisciella <stefano.pisciella1@student.univaq.it>
2022-07-13 16:08:36 +0200

    modifica ImmagineDAO

--- Collector_site/src/main/java/collector_site/data/DAO/ImmagineDAO_MySQL.java
+++ Collector_site/src/main/java/collector_site/data/DAO/ImmagineDAO_MySQL.java
@@ -8,9 +8,13 @@ package collector_site.data.DAO;
  *
  * @author stefa
  */
+import collector_site.data.impl.Genere;
+import collector_site.data.impl.Tipo;
+import collector_site.data.model.Collezione;
 import collector_site.data.model.Disco;
 import collector_site.data.model.Immagine;
 import collector_site.data.proxy.CollezionistaProxy;
+import collector_site.data.proxy.DiscoProxy;
 import collector_site.data.proxy.ImmagineProxy;
 
 // import riguardanti il framework
@@ -40,7 +44,7 @@ public class ImmagineDAO_MySQL extends DAO implements ImmagineDAO {
     private PreparedStatement deleteImmagine;
     private PreparedStatement getImmagineById;
     private PreparedStatement getImmagineByDisco;
-
+    private PreparedStatement storeImmagine;
 
     public ImmagineDAO_MySQL(DataLayer d) {
         super(d);
@@ -50,11 +54,12 @@ public class ImmagineDAO_MySQL extends DAO implements ImmagineDAO {
     public void init() throws DataException {
         try {
             super.init();
-
+            
             createImmagine = connection.prepareStatement("INSERT INTO immagine (nomeImmagine,dimensioneImmagine,filename,imgType,IDdisco) VALUES(?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
             deleteImmagine = connection.prepareStatement("DELETE FROM immagine WHERE ID=?"); 
             getImmagineById = connection.prepareStatement("SELECT * FROM immagine WHERE ID=?");
             getImmagineByDisco = connection.prepareStatement("SELECT * FROM immagine WHERE IDdisco=?");
+            storeImmagine = connection.prepareStatement("insert into immagine(nomeImmagine,imgType,IDdisco,dimensioneImmagine,filename,digest,updated) VALUES(?,?,?,?,?,?,CURRENT_TIMESTAMP);", new String[]{"ID"}); 
         } catch (SQLException ex) {
             throw new DataException("Error initializing Immagine data layer", ex);
         }
@@ -67,6 +72,7 @@ public class ImmagineDAO_MySQL extends DAO implements ImmagineDAO {
             deleteImmagine.close();
             getImmagineById.close();
             getImmagineByDisco.close();
+            storeImmagine.close();
         } catch (SQLException ex) {
         }
         super.destroy();
@@ -86,6 +92,8 @@ public class ImmagineDAO_MySQL extends DAO implements ImmagineDAO {
             immagine.setDimensioneImmagine(rs.getLong("dimensioneImmagine"));
             immagine.setFilename(rs.getString("filename"));
             immagine.setImgType(rs.getString("imgType"));
+            immagine.setDigest(rs.getString("digest"));
+            immagine.setUpdated(rs.getTimestamp("updated"));
         } catch (SQLException ex) {
             throw new DataException("Unable to create Immagine object form ResultSet", ex);
         }
@@ -145,5 +153,42 @@ public class ImmagineDAO_MySQL extends DAO implements ImmagineDAO {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-   
+    @Override
+    public void storeImmagine(Immagine immagine) throws DataException {
+          
+            try {
+            // il controllo della validità del formato dell'immagine che si vuole salvare avviene già nella
+            // servlet
+            
+            storeImmagine.setString(1, immagine.getNomeImmagine());
+            storeImmagine.setInt(2, (int) immagine.getDimensioneImmagine());
+            storeImmagine.setString(3, immagine.getFilename());
+            storeImmagine.setString(4, immagine.getImgType());
+            storeImmagine.setInt(5, immagine.getDiscoImg().getKey());
+            storeImmagine.setString(6, immagine.getDigest());
+            storeImmagine.setTimestamp(7, immagine.getUpdated());
+            
+            if (storeImmagine.executeUpdate() == 1) {
+                try ( //estrae l'ID dell'immagine caricata
+                    ResultSet keys = storeImmagine.getGeneratedKeys()) {
+                    keys.first();
+                    immagine.setKey(keys.getInt(1));
+                    //inseriamo il nuovo oggetto nella cache
+                    dataLayer.getCache().add(Immagine.class, immagine);
+                }
+               
+            }
+
+            // questo "if" deve essere eseguto sia quando si fa la create che l'update della Collezione 
+            if (immagine instanceof DataItemProxy) {
+                ((DataItemProxy) immagine).setModified(false); 
+            }
+           
+        } catch (SQLException ex) {
+            throw new DataException("Unable to store Immagine", ex);
+        } 
+            
+        // REMOVE 
+        System.out.println("ultima riga storeImmagine");
+    }
 }
